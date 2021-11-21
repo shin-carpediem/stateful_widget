e6069f2c90ad8097862db988ef7cbbac45b7f511
shin-carpediem <buru.aoshin@gmail.com>
2021-11-21 17:16:12 +0900

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
